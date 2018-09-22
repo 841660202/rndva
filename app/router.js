1cd822ffd458264e01edcdb15f8e194a8b912abb
hailong.chen <bad_100006453299_841660202@git.cloud.tencent.com>
2018-09-22 12:39:09 +0800

    init

---
+++ app/router.js
@@ -0,0 +1,147 @@
+import React, { PureComponent } from 'react'
+import { BackHandler, Animated, Easing } from 'react-native'
+import {
+  createStackNavigator,
+  createBottomTabNavigator,
+  NavigationActions,
+} from 'react-navigation'
+import {
+  reduxifyNavigator,
+  createReactNavigationReduxMiddleware,
+  createNavigationReducer,
+} from 'react-navigation-redux-helpers'
+import { connect } from 'react-redux'
+
+import Loading from './containers/Loading'
+import Login from './containers/Login'
+import Home from './containers/Home'
+import Account from './containers/Account'
+import Detail from './containers/Detail'
+
+const HomeNavigator = createBottomTabNavigator({
+  Home: { screen: Home },
+  Account: { screen: Account },
+})
+const HEADER_TITLE = {
+  Home: '首页',
+  Account: '我的',
+}
+HomeNavigator.navigationOptions = ({ navigation }) => {
+  console.log(
+    'navigation.state.routes[navigation.state.index]',
+    navigation.state.routes[navigation.state.index]
+  )
+  const { routeName } = navigation.state.routes[navigation.state.index]
+
+  return {
+    tabBarVisible: false,
+    headerTitle: HEADER_TITLE[routeName],
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
+}
+
+const MainNavigator = createStackNavigator(
+  {
+    HomeNavigator: { screen: HomeNavigator },
+    Detail: { screen: Detail },
+  },
+  {
+    headerMode: 'float',
+  }
+)
+
+const AppNavigator = createStackNavigator(
+  {
+    Main: { screen: MainNavigator },
+    Login: { screen: Login },
+  },
+  {
+    headerMode: 'none',
+    mode: 'modal',
+    navigationOptions: {
+      gesturesEnabled: false,
+    },
+    transitionConfig: () => ({
+      transitionSpec: {
+        duration: 300,
+        easing: Easing.out(Easing.poly(4)),
+        timing: Animated.timing,
+      },
+      screenInterpolator: sceneProps => {
+        const { layout, position, scene } = sceneProps
+        const { index } = scene
+
+        const height = layout.initHeight
+        const translateY = position.interpolate({
+          inputRange: [index - 1, index, index + 1],
+          outputRange: [height, 0, 0],
+        })
+
+        const opacity = position.interpolate({
+          inputRange: [index - 1, index - 0.99, index],
+          outputRange: [0, 1, 1],
+        })
+
+        return { opacity, transform: [{ translateY }] }
+      },
+    }),
+  }
+)
+
+export const routerReducer = createNavigationReducer(AppNavigator)
+
+export const routerMiddleware = createReactNavigationReduxMiddleware(
+  'root',
+  state => state.router
+)
+
+const App = reduxifyNavigator(AppNavigator, 'root')
+
+function getActiveRouteName(navigationState) {
+  if (!navigationState) {
+    return null
+  }
+  const route = navigationState.routes[navigationState.index]
+  if (route.routes) {
+    return getActiveRouteName(route)
+  }
+  return route.routeName
+}
+
+@connect(({ app, router }) => ({ app, router }))
+class Router extends PureComponent {
+  componentWillMount() {
+    BackHandler.addEventListener('hardwareBackPress', this.backHandle)
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener('hardwareBackPress', this.backHandle)
+  }
+
+  backHandle = () => {
+    const currentScreen = getActiveRouteName(this.props.router)
+    if (currentScreen === 'Login') {
+      return true
+    }
+    if (currentScreen !== 'Home') {
+      this.props.dispatch(NavigationActions.back())
+      return true
+    }
+    return false
+  }
+
+  render() {
+    const { app, dispatch, router } = this.props
+    if (app.loading) return <Loading />
+
+    return <App dispatch={dispatch} state={router} />
+  }
+}
+
+export default Router
